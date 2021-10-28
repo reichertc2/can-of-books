77708588ecdbb15fc1009c1d0b829078947a1bfb
Angela Dzodzomenyo <enyodzo@gmail.com>
2021-10-27 21:01:03 -0500

    created carousel

---
+++ src/Components/Books.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+class Books extends React.Component {
+  render() {
+    return (
+      <Carousel style={{ minHeight: '25rem' }}>
+        {this.props.books.map((el) =>
+          <Carousel.Item key={el._id}>
+            <img
+              className="d-block"
+              src="https://via.placeholder.com/150"
+              alt="First slide"
+            />
+            <Carousel.Caption style={{color: 'black'}}>
+              <h3>{el.title}</h3>
+              <p>{el.description}</p>
+            </Carousel.Caption>
+          </Carousel.Item>)}
+      </Carousel>
+    );
+  }
+}
+
+export default Books;
