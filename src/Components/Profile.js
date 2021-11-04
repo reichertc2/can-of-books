991ad04dc20e4b8bb4dd62d4ea337561e30ff9d2
Chris Reichert <chris.a.reichert@gmail.com>
2021-11-03 22:39:08 -0500

    added profile router and info

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withAuth0 } from '@auth0/auth0-react'
 
 class Profile extends React.Component {
 
@@ -6,9 +7,17 @@ class Profile extends React.Component {
     /* TODO: render information about logged in user */
     /* STRETCH TODO: if no logged in user then redirect home */
     return (
-      <p>Profile page coming soon</p>
+      <>
+        <section style={{display: 'inline-block', width:'50%'}}>
+          <h3>Welcome {this.props.auth0.user.name}, </h3>
+          <h3>{this.props.auth0.user.nickname}</h3>
+        </section>
+
+        <img src={this.props.auth0.user.picture} alt={this.props.auth0.user.name} style={{display: 'inline-block'}}></img>
+
+      </>
     );
   }
 }
 
-export default Profile;
+export default withAuth0(Profile);
